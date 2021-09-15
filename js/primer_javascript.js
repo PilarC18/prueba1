323dafe3c991cb44749bf01b5dfcefe31ea4a780
PilarC18 <pily5castillo@gmail.com>
2021-09-15 15:47:59 -0500

    3

---
+++ js/primer_javascript.js
@@ -0,0 +1,19 @@
+//alert("primerjavascript pero en el .js")
+
+var nombre="pepito perez";
+var edad=15;
+var peso= 74.5;
+var masculino=true;
+var materias;
+
+/*alert("nombre"+nombre);
+alert("peso"+peso);
+alert("mateiras"+materias);*/
+
+var numero1= prompt("ingrese numero 1");
+var numero2= prompt("ingrese numero 2");
+var resultado = parseInt (numero1) + parseInt(numero2);
+alert("El resultado de la suma es: " +resultado)
+
+// mandar codigo a html
+document.write("<p class= 'fs_1'> el resultado de la suma es:"+resultado+ "</p>");
